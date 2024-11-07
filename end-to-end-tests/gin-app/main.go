09e2525b82d1e5066c18e30ddc76c354ae0b0ca7
mirackara <mirackara2000@outlook.com>
2024-11-07 13:24:48 -0600

    multiple anon function support and bug fixes

---
+++ end-to-end-tests/gin-app/main.go
@@ -0,0 +1,66 @@
+// Copyright 2020 New Relic Corporation. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package main
+
+import (
+	"log/slog"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func anotherFunction() {
+	// dummy http request
+	_, err := http.Get("https://example.com")
+
+	if err != nil {
+		slog.Error(err.Error())
+		return
+	}
+}
+func endpointlogic(c *gin.Context) {
+	c.Writer.WriteHeader(404)
+	c.Writer.WriteString("returning 404")
+	// dummy http request
+	anotherFunction()
+}
+
+func endpoint404(c *gin.Context) {
+	c.Writer.WriteHeader(404)
+	c.Writer.WriteString("returning 404")
+	// dummy http request
+	_, err := http.Get("https://example.com")
+
+	if err != nil {
+		slog.Error(err.Error())
+		return
+	}
+}
+
+func main() {
+
+	router := gin.Default()
+
+	router.GET("/404", endpoint404)
+	router.GET("/logic", endpointlogic)
+	router.GET("/anon", func(c *gin.Context) {
+		c.Writer.WriteString("anonymous function handler")
+		// test call
+		_, err := http.Get("https://example.com")
+		if err != nil {
+			slog.Error(err.Error())
+			return
+		}
+	},
+		func(c *gin.Context) {
+			c.Writer.WriteString("anonymous function handler - second function")
+			// test call
+			_, err := http.Get("https://example.com")
+			if err != nil {
+				slog.Error(err.Error())
+				return
+			}
+		})
+	router.Run(":8000")
+}
