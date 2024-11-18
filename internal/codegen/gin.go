ffe3eb0cceefb90e9bd07d5787e346c31f004f3c
mirackara <mirackara2000@outlook.com>
2024-11-18 11:12:50 -0600

    Remove unused function

--- internal/codegen/gin.go
+++ internal/codegen/gin.go
@@ -32,30 +32,6 @@ func NrGinMiddleware(call *dst.CallExpr, routerName string, agentVariableName ds
 		},
 	}
 }
-func NrGinMiddlewareMain(call *dst.CallExpr, routerName string, agentVariableName string) *dst.ExprStmt {
-	return &dst.ExprStmt{
-		X: &dst.CallExpr{
-			Fun: &dst.SelectorExpr{
-				X:   &dst.Ident{Name: routerName},
-				Sel: &dst.Ident{Name: "Use"},
-			},
-			Args: []dst.Expr{
-				&dst.CallExpr{
-					Fun: &dst.Ident{
-						Name: "Middleware",
-						Path: NrginImportPath,
-					},
-					Args: []dst.Expr{
-						&dst.Ident{
-							Name: agentVariableName,
-						},
-					},
-				},
-			},
-		},
-	}
-}
-
 func TxnFromGinContext(txnVariable string, ctxName string) *dst.AssignStmt {
 	return &dst.AssignStmt{
 		Decs: dst.AssignStmtDecorations{
