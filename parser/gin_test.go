c04273f7c072e01b1d038fe75e7cad172967d286
mirackara <mirackara2000@outlook.com>
2024-11-19 14:13:43 -0600

    Add tests for tracing anonymous and named Gin functions

--- parser/gin_test.go
+++ parser/gin_test.go
@@ -104,3 +104,156 @@ func main() {
 		})
 	}
 }
+
+func TestGinAnonymousFunction(t *testing.T) {
+	tests := []struct {
+		name   string
+		code   string
+		expect string
+	}{
+		{
+			name: "detect and trace gin anonymous function",
+			code: `package main
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/anon", func(c *gin.Context) {
+		c.Writer.WriteString("anonymous function handler")
+		_, err := http.Get("https://example.com")
+		if err != nil {
+			return
+		}
+	},
+		func(c *gin.Context) {
+			c.Writer.WriteString("anonymous function handler - second function")
+			_, err := http.Get("https://example.com")
+			if err != nil {
+				return
+			}
+		})
+	router.Run(":8000")
+}
+`,
+			expect: `package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/newrelic/go-agent/v3/integrations/nrgin"
+)
+
+func main() {
+	router := gin.Default()
+	// NR WARN: Since the handler function name is used as the transaction name,
+	// anonymous functions do not get usefully named.
+	// We encourage transforming anonymous functions into named functions
+	router.GET("/anon", func(c *gin.Context) {
+		nrTxn := nrgin.Transaction(c)
+
+		defer nrTxn.StartSegment("/anon").End()
+		c.Writer.WriteString("anonymous function handler")
+		_, err := http.Get("https://example.com")
+		if err != nil {
+			return
+		}
+	},
+		func(c *gin.Context) {
+			nrTxn := nrgin.Transaction(c)
+
+			defer nrTxn.StartSegment("/anon-2").End()
+			c.Writer.WriteString("anonymous function handler - second function")
+			_, err := http.Get("https://example.com")
+			if err != nil {
+				return
+			}
+		})
+	router.Run(":8000")
+}
+`,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			defer panicRecovery(t)
+			got := testStatelessTracingFunction(t, tt.code, FindAnonymousFunctions)
+			assert.Equal(t, tt.expect, got)
+		})
+	}
+}
+
+func TestInstrumentGinFunction(t *testing.T) {
+	tests := []struct {
+		name   string
+		code   string
+		expect string
+	}{
+		{
+			name: "detect and trace gin function",
+			code: `package main
+
+import (
+	"log/slog"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func HandleGinFunction(c *gin.Context) {
+	c.Writer.WriteString("anonymous function handler")
+	// test call
+	_, err := http.Get("https://example.com")
+	if err != nil {
+		slog.Error(err.Error())
+		return
+	}
+}
+func main() {
+	router := gin.Default()
+	router.GET("/anon", HandleGinFunction)
+	router.Run(":8000")
+}
+`,
+			expect: `package main
+
+import (
+	"log/slog"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/newrelic/go-agent/v3/integrations/nrgin"
+)
+
+func HandleGinFunction(c *gin.Context) {
+	nrTxn := nrgin.Transaction(c)
+
+	c.Writer.WriteString("anonymous function handler")
+	// test call
+	_, err := http.Get("https://example.com")
+	if err != nil {
+		nrTxn.NoticeError(err)
+		slog.Error(err.Error())
+		return
+	}
+}
+func main() {
+	router := gin.Default()
+	router.GET("/anon", HandleGinFunction)
+	router.Run(":8000")
+}
+`,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			defer panicRecovery(t)
+			got := testStatelessTracingFunction(t, tt.code, InstrumentGinFunction)
+			assert.Equal(t, tt.expect, got)
+		})
+	}
+}
