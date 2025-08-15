9bae766637e4a8c80187c740e385f3a32384ca98
Brian Duranleau <bduranleau@newrelic.com>
2025-08-15 15:52:59 -0500

    test: add Chi Literal test

--- parser/gochi_test.go
+++ parser/gochi_test.go
@@ -190,3 +190,67 @@ func TestChiMiddlewareCall(t *testing.T) {
 		})
 	}
 }
+
+func TestInstrumentChiLiteral(t *testing.T) {
+	tests := []struct {
+		name   string
+		code   string
+		expect string
+	}{
+		{
+			name: "Instrument Chi Literal in http method handler",
+			code: `package main
+import (
+	"net/http"
+
+	chi "github.com/go-chi/chi/v5"
+)
+
+func main() {
+	router := chi.NewRouter()
+	router.Get("/literal", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome"))
+	})
+	http.ListenAndServe(":3000", router)
+}`,
+			expect: `package main
+
+import (
+	"net/http"
+	"time"
+
+	chi "github.com/go-chi/chi/v5"
+	"github.com/newrelic/go-agent/v3/newrelic"
+)
+
+func main() {
+	NewRelicAgent, agentInitError := newrelic.NewApplication(newrelic.ConfigFromEnvironment())
+	if agentInitError != nil {
+		panic(agentInitError)
+	}
+
+	router := chi.NewRouter()
+	router.Get("/literal", func(w http.ResponseWriter, r *http.Request) {
+		nrTxn := newrelic.FromContext(r.Context())
+
+		defer nrTxn.StartSegment("GET:/literal").End()
+
+		w.Write([]byte("welcome"))
+	})
+	http.ListenAndServe(":3000", router)
+
+	NewRelicAgent.Shutdown(5 * time.Second)
+}
+`,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			defer panicRecovery(t)
+			got := testStatelessTracingFunction(t, tt.code, InstrumentMain, InstrumentChiLiteral)
+			assert.Equal(t, tt.expect, got)
+		})
+	}
+
+}
